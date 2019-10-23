5d2d2e80ae8060ab7bcb2f9c2a47a1aa56dc420b
paulusang <paulus.ang@gmail.com>
2019-10-23 16:04:44 +1100

    Trying various style attributes

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
 export default function LinksScreen() {
+  
+/**  constructor(props) {
+    super(props);
+    this.state={text: ''};
+  }
+         <TextInput
+            style={{height: 40}}
+            placeholder="Type here to translate!"
+            onChangeText={(text)=>this.setState({text})}
+            value={this.state.text}
+          />
+          <Text style={{padding:10, fontSize:42}}>
+            {this.state.next.split(' ').map((word)=>word && '🍕').join(' ')}
+          </Text>
+          
+  */
   return (
     <ScrollView style={styles.container}>
       {/**
@@ -10,9 +26,17 @@ export default function LinksScreen() {
        * we just wanted to provide you with some helpful links.
        */}
       <ExpoLinksView />
-      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-      <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      <View style={{
+        height:450, 
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        }}>
+        <View style={{width:50,height:50, padding:10, alignSelf:'center',backgroundColor: 'powderblue'}}>
+        </View>
+        <View style={{width:50,height:50, backgroundColor: 'skyblue'}} />
+        <View style={{width:50,height:100, backgroundColor: 'steelblue'}} />
+      </View>
     </ScrollView>
   );
 }
